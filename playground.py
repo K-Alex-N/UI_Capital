a3e72ab8fb1c0b866a305dd45e89805dda70a397
Алексей <KurochkinAlexei@yandex.ru>
2024-07-29 21:26:58 +0200

    init

---
+++ playground.py
@@ -0,0 +1,15 @@
+bc_name, title_name = {
+    "ar": ['تطبيقاتنا للهواتف المحمولة', 'تطبيقات جوّال Capital.com'],
+    "de": ['Unsere mobile App', 'Capital.com: Mobile Apps'],
+    "el": ['Τρόποι να διαπραγματευτείτε', 'Τρόποι να διαπραγματευτείτε'],
+    "es": ['Nuestras aplicaciones móviles', 'Aplicaciones móviles de Capital.com'],
+    "fr": ['Nos applis mobiles', 'Applications mobiles de Capital.com'],
+    "it": ['Le nostre app mobili', 'App per dispositivi mobili di Capital.com'],
+    "hu": ['Mobilalkalmazásunk', 'Capital.com mobilalkalmazások'],
+    "nl": ['Onze mobiele apps', 'Capital.com mobiele apps'],
+    "pl": ['Nasze aplikacje mobilne', 'Aplikacje mobilne Capital.com'],
+    "ro": ['Aplicațiile noastre mobile', 'Aplicații mobile Capital.com'],
+    "ru": ['Наше мобильное приложение', 'Мобильные приложения Capital.com'],
+    "zh": ['交易方式', '交易方式'],
+    "cn": ['我们的移动应用程序', 'Capital.com 移動應用程序'],
+}.get(cur_language, ("", ""))
